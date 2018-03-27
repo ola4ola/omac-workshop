caee8dfdd01356a780e7475d719824334dff97b3
ola <ola.darwish@yahoo.com>
2018-03-27 22:41:32 +0300

    adjust for multibank

--- atm soultion/atm.py
+++ atm soultion/atm.py
@@ -1,29 +1,44 @@
 # allowed papers: 100, 50, 10, 5, and rest of request
-
-balance = 500
-
 valid_currency = [100, 50, 10, 5]
 
-def withdraw (balance, request):
-	print 'current balance =' + str(balance)
-	
-	if request > balance:
-		print 'no enough balance'
-		return 0
-	elif request <= 0 :
-		print ' you should enter a request more than zero'
-		return 0
-	
-	remain = balance - request
-	for currency in valid_currency:
-		while request >= currency:
-			print 'give' + str(currency)
-			request -= currency
-	if request > 0:
-		print 'give' + str(request)
-	return remain
+class ATM:
+	def __init__(self, balance, bank_name):
+		self.balance = balance
+		self.bank_name = bank_name
+	def withdraw (self, request):
+		print 'Welcome to' + self.bank_name
+		print 'current balance =' + str(self.balance)
+		print '==========================='
+		
+		if request > self.balance:
+			print 'no enough balance'
+			print '======================='
+			return 0
+		elif request <= 0 :
+			print ' you should enter a request more than zero'
+			return 0
+		
+		remain = self.balance - request
+		for currency in valid_currency:
+			while request >= currency:
+				print 'give ' + str(currency)
+				request -= currency
+		if request > 0:
+			print 'give ' + str(request)
+		print'==================================='
+		return remain
 
-balance = withdraw(balance, 277)
-balance = withdraw(balance, 30)
-balance = withdraw(balance, 5)
-balance = withdraw(balance, 500)
+balance1 = 500
+balance2 = 1000
+
+atm1 = ATM(balance1, "Smart Bank")
+atm2 = ATM(balance2, "Baraka Bank")
+
+atm1.withdraw(277)
+atm1.withdraw(800)
+
+atm2.withdraw(100)
+atm2.withdraw(2000)
+
+
+

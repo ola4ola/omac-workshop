2ce2450aefd91776d49806063962427809296793
ola <ola.darwish@yahoo.com>
2018-03-29 09:47:42 +0300

    add receipt

--- atm soultion/atm.py
+++ atm soultion/atm.py
@@ -5,6 +5,7 @@ class ATM:
 	def __init__(self, balance, bank_name):
 		self.balance = balance
 		self.bank_name = bank_name
+		self.withdrawal_list = []
 	def withdraw (self, request):
 		print 'Welcome to' + self.bank_name
 		print 'current balance =' + str(self.balance)
@@ -17,16 +18,22 @@ class ATM:
 		elif request <= 0 :
 			print ' you should enter a request more than zero'
 			return 0
-		
-		remain = self.balance - request
-		for currency in valid_currency:
-			while request >= currency:
-				print 'give ' + str(currency)
-				request -= currency
-		if request > 0:
-			print 'give ' + str(request)
-		print'==================================='
-		return remain
+		else:
+			self.withdrawal_list.append(request)
+			self.balance -= request
+			for currency in valid_currency:
+				while request >= currency:
+					print 'give ' + str(currency)
+					request -= currency
+			if request > 0:
+				print 'give ' + str(request)
+			print'==================================='
+			return self.balance
+	def show_withdrawal(self):
+		i = 0
+		for withdrawal in self.withdrawal_list:
+			i += 1
+			print 'withdrawal operation ' + str(i) + ' : '+ str(withdrawal)
 
 balance1 = 500
 balance2 = 1000
@@ -36,9 +43,10 @@ atm2 = ATM(balance2, "Baraka Bank")
 
 atm1.withdraw(277)
 atm1.withdraw(800)
+atm1.show_withdrawal()
 
 atm2.withdraw(100)
 atm2.withdraw(2000)
-
+atm2.show_withdrawal()
 
 
